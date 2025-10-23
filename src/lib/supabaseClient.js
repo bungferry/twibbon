c5ab3ceac34ef9a6deedc03107fb89ae3386b960
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2025-10-23 13:53:33 +0700

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 // lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL' // Ganti dengan URL proyek Anda
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY' // Ganti dengan Public Key Anda
+const supabaseUrl = 'https://hruicocxjqpabjlzwyfg.supabase.co' // Ganti dengan URL proyek Anda
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhydWljb2N4anFwYWJqbHp3eWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExOTM1MDgsImV4cCI6MjA3Njc2OTUwOH0.KQIKWguGonN36nzhhjaQNBHEL8-gRoH8i4xi2nnkDSg' // Ganti dengan Public Key Anda
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
