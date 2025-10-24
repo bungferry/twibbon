0f6a12ca0f23a1e1e70aae35f0892eda48966eef
Ferry Ayunda <79414671+bungferry@users.noreply.github.com>
2025-10-24 16:57:54 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
     <div class="toolbar">
       <small class="info">
         <span v-if="imageLoaded"> Seret untuk memindahkan. Cubit dua jari untuk memperbesar. Scroll untuk zoom (desktop). </span>
-        <span v-else> Silakan unggah gambar Anda di atas untuk mulai mengedit. </span>
+        <span v-else> Silakan unggah gambar terbaik Anda di atas untuk mulai mengedit. </span>
       </small>
     </div>
   </div>
